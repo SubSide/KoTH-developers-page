324aa360a9c631f43cf19f99a7af62068204799c
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2016-12-24 19:17:04 +0100

    Basic index/style/htaccess setup
    Basic index.php and the style.css
    Also the .htaccess that will be used for linking

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+$page = empty($_GET["path"]) ? "home" : $_GET["path"];
+$page = file_exists(dirname(__FILE__) . '/content/' . $page . ".html") ? $page : "home";
+
+if(!preg_match("/^([a-z0-9\/]+)/i", $page)){
+	$page = "home";
+}
+?>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="Thomas \"SubSide\" van den Bulk">
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300' rel='stylesheet' type='text/css'>
+	<link href="/KoTH/css/style.css" rel="stylesheet" type="text/css">
+	<title>KoTH info page</title>
+</head>
+<body>
+<div class="wrapper">
+	<aside>
+		<div>
+			<a href="/KoTH/">Home</a>
+		</div>
+		<br />
+		<div class="menu-item">
+			<span class="menu-control">Commands</span>
+			<div class="menu">
+				<a href="/KoTH/commands/basic">Basic commands</a>
+				<a href="/KoTH/commands/control">Control commands</a>
+				<div class="menu-item">
+					<span class="menu-control">Editor commands</span>
+					<div class="menu">
+						<a href="/KoTH/commands/editor/basic">Basic editor commands</a>
+						<a href="/KoTH/commands/editor/edit">Koth editor commands</a>
+						<a href="/KoTH/commands/editor/loot">Loot commands</a>
+						<a href="/KoTH/commands/editor/schedule">Schedule commands</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="menu-item">
+			<span class="menu-control">Config</span>
+			<div class="menu">
+				<a href="/KoTH/config/config">config.yml explained</a>
+				<a href="/KoTH/config/koths">Koths.json explained</a>
+				<a href="/KoTH/config/loot">Loot.json explained</a>
+				<a href="/KoTH/config/schedule">Schedule.json explained</a>
+				<a href="/KoTH/config/lang">Lang.json</a>
+			</div>
+		</div>
+		<br />
+		<div>
+			<a href="./javadocs/">Developers API</a>
+		</div>
+	</aside>
+	<main>
+		<?php
+		include_once(dirname(__FILE__) . '/content/' . $page . ".html");
+		?>
+	</main>
+</div>
+</body>
+</html>
